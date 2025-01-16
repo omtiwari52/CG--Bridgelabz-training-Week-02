5451c2b480e3f2eaecc5da1174b9e297c8bc842e
omtiwari52 <tiwariom900@gmail.com>
2025-01-16 15:28:13 +0530

    Day01 Done

---
+++ Day01/Level01/Book.java
@@ -0,0 +1,33 @@
+// Write a program to create a Book class with attributes title, author, and price. Add a method to display the book details.
+
+package Day01.Level01;
+
+public class Book {
+    
+    //Create attributes 
+    String title;
+    String author;
+    int price;
+
+    //Create a constructor
+    Book(String title, String author, int price){
+        this.title = title;
+        this.author = author;
+        this.price = price;
+    }
+
+    //Display details 
+    void displayDetails(){
+        System.out.println("Title: " + title);
+        System.out.println("Author: " + author);
+        System.out.println("Price: " + price);
+    }
+
+    public static void main(String[] args) {
+        
+        //Create an object of the Book class
+        Book book = new Book("Death : An Inside Story", "Sadhguru", 250);
+        book.displayDetails();
+
+    }
+}
